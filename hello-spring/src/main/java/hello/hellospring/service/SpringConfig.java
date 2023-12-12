0244ab416bfeb9a9e12dfdb1d5d9054e73d799fd
Jin <kpu9939@gmail.com>
2023-12-12 20:06:47 +0900

    upload

--- hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
-//;
+// ;
 @Configuration
 public class SpringConfig {
 
